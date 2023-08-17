b9c89754c8b943e25c7bef5617b641b86ef157ba
Linehb <linehaaber@hotmail.com>
2023-08-17 16:33:03 +0200

    Theme

--- script.js
+++ script.js
@@ -12,15 +12,30 @@ document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
 
     if (scroll_position > 250) {
-        header.style.backgroundColor = '#755f94';
+        header.style.backgroundColor = 'hsl(180, 50%, 50%)';
     } else {
         header.style.backgroundColor = 'transparent';
     }
 });
-
 navigation_data.forEach((item) => {
     item.addEventListener('click', () => {
         burger.classList.toggle('bar');
         mobile_burger.classList.toggle('bar');
     });
 });
+
+function myFunction() {
+    var dots = document.getElementById('dots');
+    var moreText = document.getElementById('more');
+    var btnText = document.getElementById('myBtn');
+  
+    if (dots.style.display === 'none') {
+      dots.style.display = 'inline';
+      btnText.innerHTML = 'Show Again';
+      moreText.style.display = 'none';
+    } else {
+      dots.style.display = 'none';
+      btnText.innerHTML = 'Show Less'; 
+      moreText.style.display = 'inline';
+    }
+  }
